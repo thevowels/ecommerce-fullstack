9589b3927815dce56d137a45f695795ccc00d00c
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-03-19 22:50:23 +0700

    feat: curd for category.

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -2,39 +2,73 @@ import { prisma, Prisma } from "@repo/product-db";
 import { Request, Response } from "express";
 
 export const createCategory = async (req: Request, res: Response) => {
-		const data: Prisma.CategoryCreateInput = req.body;
-		const category = await prisma.category.create({data});
-		res.status(201).json({
-			data: category
-		});
-	
+  const data: Prisma.CategoryCreateInput = req.body;
+  const category = await prisma.category.create({ data });
+  res.status(201).json({
+    data: category,
+  });
 };
 
 export const updateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
+  const data: Prisma.CategoryUpdateInput = req.body;
+
+  try {
+    const updatedCategory = await prisma.category.update({
+      where: {
+        category_id: Number(id),
+      },
+      data,
+    });
+    res.status(201).json({
+      message: "updated",
+      data: updatedCategory,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError)
+      if (error.code === "P2025")
+        return res.status(404).json({ message: "Record not found!" });
+    return res.status(500).send();
+  }
 };
 
 export const deleteCategory = async (req: Request, res: Response) => {
-	
+  const { id } = req.params;
+
+  try {
+    await prisma.category.delete({
+      where: {
+        category_id: Number(id),
+      },
+    });
+    return res.status(204).send();
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025")
+        res.status(404).json({ message: "record not found" });
+    }
+
+    res.status(500).send();
+  }
 };
 
 export const getCategories = async (req: Request, res: Response) => {
-	const categories = await prisma.category.findMany();
+  const categories = await prisma.category.findMany();
 
-	res.json({message:"success", categories});
+  res.json({ message: "success", categories });
 };
 export const getCategory = async (req: Request, res: Response) => {
-	const {id} = req.params;
+  const { id } = req.params;
 
-	const product = await prisma.category.findUnique({
-		where:{
-			category_id: Number(id)
-		}
-	})
-	if(!product)
-			return res.status(404).send();
-	return res.status(200).json({
-		message:"success",
-		data: product
-	})
+  const product = await prisma.category.findUnique({
+    where: {
+      category_id: Number(id),
+    },
+  });
+  if (!product) return res.status(404).send();
+  return res.status(200).json({
+    message: "success",
+    data: product,
+  });
 };
